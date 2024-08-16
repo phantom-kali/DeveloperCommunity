fb7c1f9e734198a0b86a6cf6663bd47ce862d0e2
fideleliud <fideleliudclimax@gmail.com>
2024-08-16 09:16:46 +0300

    changed report error to share error

--- errors/urls.py
+++ errors/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.error_list, name='error_list'),
-    path('create/', views.create_error, name='report_error'),
+    path('create/', views.create_error, name='share_error'),
     path('<int:pk>/', views.error_detail, name='error_detail'),
     path('<int:pk>/edit/', views.edit_error, name='edit_error'),
     path('<int:pk>/delete/', views.delete_error, name='delete_error'),
